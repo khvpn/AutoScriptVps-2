81f537f385a3d9e8bdc1981246b35f372bd150ba
khvpn <chopavpn@gmail.com>
2021-07-04 14:42:56 +0700

    Initial commit

---
+++ Files/Menu/clearcache.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Script by : Gugun09
+echo "echo 3 > /proc/sys/vm/drop_caches"
+clear
+echo -e "\e[0m                                                   "
+echo -e "\e[94m[][][]======================================[][][]"
+echo -e "\e[0m                                                   "
+echo -e "\e[93m                  Cache  Cleared                  "
+echo -e "\e[0m                                                   "
+echo -e "\e[94m[][][]======================================[][][]\e[0m"
